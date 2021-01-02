7d2bd60eb0cdb7e91c464b1bc0afb6d21d74a307
am401 <andras.marton@wpengine.com>
2021-01-02 17:15:29 -0600

    Updated logic of the game and added change log to README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # guess_num
-Simple python game to guess a randomly generated number
+This is a simple game that I started to write to improve my Python usage. The game overall is extremely simple, a random number is generated between zero and nine and you have three attempts to guess the correct number.
+
+# Change Log
+## [ 0.1 ] 2021-01-02
+### Added
+- Added instruction banner when playing the game to provide the range the random number will be generated in
+### Change
+- Moved somoe of the logic from different functions under __main__
+- Changed from using `system` to `sys.exit` when exiting the program
+- Changed the screen clear to directly use the `os` module
+### Removed
+- Removed the main() function moving the content to __main__ or guess_the_number()
--- guess.py
+++ guess.py
@@ -1,67 +1,73 @@
-""" Random number guessing game
-Author: Andras Marton
-Date: 4th January 2020"""
-
 import random
-from os import system
-import textwrap
+import os
+import sys
+
 
 class color:
-	BOLD = '\033[1m'
-	WHITE = '\033[37m'
-	CYAN = '\033[96m'
-	RED = '\033[91m'
-	GREEN = '\033[92m'
-	END = '\033[0m'
-	YELLOW = '\033[33m'
+    BOLD = '\033[1m'
+    WHITE = '\033[37m'
+    CYAN = '\033[96m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    END = '\033[0m'
+    YELLOW = '\033[33m'
+
 
 def clear():
-	"""Clear the screen at the start of the script"""
-	_ = system('clear')
+    """Clear the screen at the start of the script"""
+    _ = os.system('clear')
 
-def actual_number_message():
-	print(color.BOLD + "\nOops.. the actual number was " + color.CYAN + str(num) + color.WHITE + "..." + color.END)
 
-clear()
+def header():
+    print("+" * 10 + " Main Menu " + "+" * 10 + "\n")
+    print("1) Play the game\n2) Score board\n3) Exit\n")
 
-#  Set the number of attempts the user has to guess
-n = 4
-i = 1
-hello_message = """Howdy! Welcome to the game of Guess The Number. You guessed it right...
-The aim of the game is to guess the magic number. You have three chances
-to do so. Good luck!
-"""
 
-for line in (textwrap.wrap(hello_message, width = 70)):
-	print(color.BOLD + line + color.END)
+def guess_the_number(guess, num):
+    terminate = ("q","quit","sys.exit")
+    try:
+        if guess in terminate:
+            print("\nSee ya another time!")
+            sys.exit()
+        else:
+            guess = int(guess)
+            if num == guess:
+                print(color.GREEN + "Well done, your guess was right." + color.END)
+                print("\nHowever you win nothing. Thank you for playing.")
+                sys.exit()
+            else:
+                print(color.RED + "Oh no your guess is wrong." + color.END)
+    except ValueError:
+            try:
+                float(guess)
+                print(color.YELLOW + "Please only use integers!" + color.END)
+            except ValueError:
+                print(color.YELLOW + "This is not a number! Get it together!" + color.END)
 
-num = random.randint(0,9)
-terminate = ("q","quit","exit")
 
-while i < n:
-	try:
-		guess = input(color.BOLD + "\nEnter your guess: " + color.END)
-		if guess in terminate:
-			print("\nSee ya another time!")
-			break
+if __name__ == '__main__':
+    clear()
+    header()
+    while True:
+        choice = input("Please enter your choice: ")
+        if choice == "1":
+            n = 4
+            i = 1
+            num = random.randint(0,9)
+            print("Guess the number to win! The number will be between " + color.YELLOW + "0" + color.END + " and " + color.YELLOW + "9" + color.END + "!")
+            while i < n:
+                guess = input(color.BOLD + "\nEnter your guess: " + color.END)
+                guess_the_number(guess, num)
+                i = i + 1
+            else:
+                print(color.BOLD + "\nOops.. the actual number was " + color.CYAN + str(num) + color.WHITE + "...\n" + color.END)
 
-		else:
-			guess = int(guess)
-			if num == guess:
-				print(color.GREEN + "Well done, your guess was right." + color.END)
-				print("\nHowever you win nothing. Thank you for playing.")
-				break
-			else:
-				print(color.RED + "Oh no you guessed it wrong." + color.END)
-			#print(color.CYAN + "The number is: " + str(num) + color.END)
-			#print(color.CYAN + "Your guess was: " + str(guess) + color.END)
-	except ValueError:
-		try:
-			float(guess)
-			print(color.YELLOW + "Please only use integers!" + color.END)
-			continue
-		except ValueError:
-			print(color.YELLOW + "This is not a number! Get it together!" + color.END)
-	i = i + 1
-else:
-	print(color.BOLD + "\nOops.. the actual number was " + color.CYAN + str(num) + color.WHITE + "..." + color.END)
+        elif choice == "2":
+            print("This is coming soon...")
+        elif choice =="3":
+            #sys.exit()
+            break
+        else:
+            print("Incorrect input detected: " + choice + " is not a valid response")
+            print("Please try again.")
+        header()
